dbd7ca753e58a1fe35c83783326bd581bff63dcb
teshager habtie <teshie00001@gmail.com>
2025-01-29 07:40:51 -0600

    this is checking auth if user is authenticated

--- JWT/controllers/auth.controller.js
+++ JWT/controllers/auth.controller.js
@@ -51,37 +51,51 @@ export const signup = async (req, res) => {
 };
 
 export const login = async (req, res) => {
-
-    const { email, password } = req.body;
-    try {
-        if (!email || !password) {
-        return res.status(404).send("all input is required");
-        }
-        const existEmail = await users.findOne({ email });
-        if (!existEmail) {
-        return res.status(404).send("email is not existing required");
-        }
-        const validPassword = await bcrypt.compare(password, existEmail.password);
-        if (!validPassword) {
-        return res.status(404).send("password is not valid required");
-        }
-        generatTookenandsetcookies(res, existEmail._id);
-        res.status(201).json({
-        success: true,
-        message: "user is success fuly login",
-        User: {
-            ...existEmail._doc,
-            password: undefined,
-        },
-        });
-    } catch (error) {
-        res.status(404).json({
-        success: false,
-        message: error.message,
-        });
+  const { email, password } = req.body;
+  try {
+    if (!email || !password) {
+      return res.status(404).send("all input is required");
     }
+    const existEmail = await users.findOne({ email });
+    if (!existEmail) {
+      return res.status(404).send("email is not existing required");
+    }
+    const validPassword = await bcrypt.compare(password, existEmail.password);
+    if (!validPassword) {
+      return res.status(404).send("password is not valid required");
+    }
+    generatTookenandsetcookies(res, existEmail._id);
+    res.status(201).json({
+      success: true,
+      message: "user is success fuly login",
+      User: {
+        ...existEmail._doc,
+        password: undefined,
+      },
+    });
+  } catch (error) {
+    res.status(404).json({
+      success: false,
+      message: error.message,
+    });
+  }
 };
 
 export const checkAuth = async (req, res) => {
-    
+  try {
+    const user = await users.findById(req.user_id);
+    res.status(200).json({
+      success: true,
+      message: "user is authenticated",
+      User: {
+        ...user._doc,
+        password: undefined,
+      },
+    });
+  } catch (error) {
+    res.status(404).json({
+      success: false,
+      message: error.message,
+    });
+  }
 };
--- JWT/route/auth.js
+++ JWT/route/auth.js
@@ -1,9 +1,12 @@
 import express from "express";
 import { checkAuth, signup, login } from "../controllers/auth.controller.js";
+import dotenv from "dotenv";
+dotenv.config();
+import { verifyToken } from "./../middleware/verifywebtoken.js";
 const router = express.Router();
 
 router.post("/signup", signup);
 router.get("/login", login);
-router.get("/check-auth", checkAuth);
+router.get("/check-auth", verifyToken, checkAuth);
 
 export default router;
