cfee97d424e4517cae98562b418ac0058b3cc60c
teshager habtie <teshie00001@gmail.com>
2025-01-29 06:56:58 -0600

    creating user schema

---
+++ JWT/models/userschema.js
@@ -0,0 +1,23 @@
+import { types } from "mime-types";
+import mongoose from "mongoose";
+
+const userscema = new mongoose.Schema(
+  {
+    username: {
+      type: String,
+      required: true,
+    },
+    email: {
+      type: String,
+      required: true,
+      unique: true,
+    },
+    password: {
+      type: String,
+      required: true,
+    },
+  },
+  { timestamps: true }
+);
+
+export const users = mongoose.model("users", userscema);
