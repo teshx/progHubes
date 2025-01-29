17198888825fb65615dd9927eadd686e506360ca
teshager habtie <teshie00001@gmail.com>
2025-01-29 07:33:43 -0600

    creating login

--- JWT/controllers/auth.controller.js
+++ JWT/controllers/auth.controller.js
@@ -3,6 +3,7 @@ import dotenv from "dotenv";
 import jwt from "jsonwebtoken";
 import bcrypt from "bcrypt";
 import { users } from "../model/Userschema.js";
+import { generatTookenandsetcookies } from "../utills/generatewebtoken.js";
 
 export const signup = async (req, res) => {
   const { username, email, password } = req.body;
@@ -23,19 +24,16 @@ export const signup = async (req, res) => {
 
     const salt = await bcrypt.genSalt(10);
     const hashedPassword = await bcrypt.hash(password, salt);
-    //verificatiionToken
 
     const User = new users({
+      username,
       email,
       password: hashedPassword,
-      username,
-      verificationToken,
-      verificationExpires: Date.now() + 24 * 60 * 60 * 1000, // 24 houres
     });
 
     await User.save();
     //jwt
-
+    generatTookenandsetcookies(res, User._id);
     res.status(201).json({
       success: true,
       message: "user is success fuly created",
@@ -52,14 +50,38 @@ export const signup = async (req, res) => {
   }
 };
 
-
-
-
-
 export const login = async (req, res) => {
- 
+
+    const { email, password } = req.body;
+    try {
+        if (!email || !password) {
+        return res.status(404).send("all input is required");
+        }
+        const existEmail = await users.findOne({ email });
+        if (!existEmail) {
+        return res.status(404).send("email is not existing required");
+        }
+        const validPassword = await bcrypt.compare(password, existEmail.password);
+        if (!validPassword) {
+        return res.status(404).send("password is not valid required");
+        }
+        generatTookenandsetcookies(res, existEmail._id);
+        res.status(201).json({
+        success: true,
+        message: "user is success fuly login",
+        User: {
+            ...existEmail._doc,
+            password: undefined,
+        },
+        });
+    } catch (error) {
+        res.status(404).json({
+        success: false,
+        message: error.message,
+        });
+    }
 };
 
-
-
-export const checkAuth = async (req, res) => {};
+export const checkAuth = async (req, res) => {
+    
+};
--- JWT/route/auth.js
+++ JWT/route/auth.js
@@ -2,7 +2,7 @@ import express from "express";
 import { checkAuth, signup, login } from "../controllers/auth.controller.js";
 const router = express.Router();
 
-router.get("/signp", signup);
+router.post("/signup", signup);
 router.get("/login", login);
 router.get("/check-auth", checkAuth);
 
