da9fa964a6e133d19bde6514905d7af2fb7fdfd1
teshager habtie <teshie00001@gmail.com>
2025-01-29 05:55:16 -0600

    configer

--- API/index.js
+++ API/index.js
@@ -1,5 +1,6 @@
 import express from "express";
 import connectdb from "./db/connectDB.js";
+import router from "./routes/auth.js";
 import bodyParser from "body-parser";
 import cors from "cors";
 import cookieParser from "cookie-parser";
@@ -9,9 +10,13 @@ const app = express();
 dotenv.config();
 const PORT = process.env.port || 4000;
 
-app.get("/", (req, res) => {
-  res.send("Hello World");
-});
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
+
+app.use(express.json()); //allow paree in comming requestes:req.body
+app.use(cookieParser()); // it allow parse incoming cookies
+app.use("/", router);
+app.use(cors());
 app.listen(PORT, () => {
   connectdb();
   console.log(`this is server is runing on port http://localhost:${PORT}`);
